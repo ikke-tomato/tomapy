cc309a429c9808f0e8eac3a8689868e7cdde8698
2015 MacBook <2015macbook@MacBook-Air.local>
2021-11-12 20:52:15 +0900

    make it better

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('qu0Qt+nG4ip+ddSi0lGTcURXjeZbbW4HKcr5fHpBp1quuu4wwQEm+timkCOK0WGTUmbk92/j0ulF/v5rubGerfv5YJVhdkMjp4OuesInGAN0MllPY1QaF8COpr7a+gJx5rOC4lpQWn4N37PdQiITDQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('sKnbqRQApoN2C6TW5/MSVM2/QB4w4tqojJf+qbq3V1G18VbbULl10eMWn+94qoWsUmbk92/j0ulF/v5rubGerfv5YJVhdkMjp4OuesInGAMTRQuGbg/HEQMKI6RMZKk2JuJYcbiRAiHambHYv0CHKwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('4618cba20c09232e59c0a554dab4c645')
 
 @app.route("/")
@@ -35,14 +35,11 @@ def callback():
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
-    return 'OK'
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="あなたは" + event.message.text + "と言いました"))
+        TextSendMessage(text="your message is " + event.message.text + " ."))
          
 
 if __name__ == "__main__":
